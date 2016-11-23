957d9cb261471b2776ab1c9485f8a3bfccbd3a3b
Jakub Karbowiak <jakub.karbowiak@capgemini.com>
2016-11-23 22:39:36 +0100

    initialized sbt project structure

---
+++ src/main/scala/com/aksoft/scala/hello/HelloWorld.scala
@@ -0,0 +1,11 @@
+package com.aksoft.scala.hello
+
+/**
+  * Created by jkarbowi on 23.11.2016.
+  */
+object HelloWorld {
+
+  def main(args: Array[String]): Unit = {
+    printf("Hello World")
+  }
+}
