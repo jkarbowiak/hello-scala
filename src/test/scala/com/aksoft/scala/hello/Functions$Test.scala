1d83057e2c18dc549400567e37227c10ed21d53c
Jakub Karbowiak <jakub.karbowiak@capgemini.com>
2016-12-13 21:26:29 +0100

    test refactored to FunSpec

--- src/test/scala/com/aksoft/scala/hello/Functions$Test.scala
+++ src/test/scala/com/aksoft/scala/hello/Functions$Test.scala
@@ -1,42 +1,47 @@
 package com.aksoft.scala.hello
 
-import org.scalatest.FunSuite
+import org.scalatest.FunSpec
 
 /**
   * Created by jkarbowi on 13.12.2016.
   */
-class Functions$Test extends FunSuite {
+class Functions$Test extends FunSpec {
 
-  test("powerOfTwo should return 1 for 0") {
-    assert(Functions.powerOfTwo(0) == 1)
+  describe("powerOfTwo") {
+    it("should return 1 for 0") {
+      assert(Functions.powerOfTwo(0) == 1)
+    }
+
+    it("should return 2 for 1") {
+      assert(Functions.powerOfTwo(1) == 2)
+    }
+
+    it("should return 4 for 2") {
+      assert(Functions.powerOfTwo(2) == 4)
+    }
+
+    it("should return 8 for 3") {
+      assert(Functions.powerOfTwo(3) == 8)
+    }
   }
 
-  test("powerOfTwo should return 2 for 1") {
-    assert(Functions.powerOfTwo(1) == 2)
+  describe("factorial") {
+    it("should return 1 for 0") {
+      assert(Functions.factorial(0) == 1)
+    }
+
+    it("should return 1 for 1") {
+      assert(Functions.factorial(1) == 1)
+    }
+
+    it("should return 2 for 2") {
+      assert(Functions.factorial(2) == 2)
+    }
+
+    it("should return 6 for 3") {
+      assert(Functions.factorial(3) == 6)
+    }
   }
 
-  test("powerOfTwo should return 4 for 2") {
-    assert(Functions.powerOfTwo(2) == 4)
-  }
-
-  test("powerOfTwo should return 8 for 3") {
-    assert(Functions.powerOfTwo(3) == 8)
-  }
-
-  test("factorial should return 1 for 0") {
-    assert(Functions.factorial(0) == 1)
-  }
-
-  test("factorial should return 1 for 1") {
-    assert(Functions.factorial(1) == 1)
-  }
-
-  test("factorial should return 2 for 2") {
-    assert(Functions.factorial(2) == 2)
-  }
-
-  test("factorial should return 6 for 3") {
-    assert(Functions.factorial(3) == 6)
-  }
 
 }
